66a7a89f516be3be005952cad5ac59f2ede48044
Itzkrish333 <96968818+Itzkrish333@users.noreply.github.com>
2022-04-26 11:21:37 +0530

    Create pr-2.cpp

---
+++ pr-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    int c,d,e,f,g;
+    
+   cout<<" enter first number:";
+   cin>>a;
+   cout<<"enter second numer:";
+   cin>>b;
+   c=a+b;
+   d=a-b;
+   e=a*b;
+   f=a/b;
+   g=a%b;
+   cout<<"addition ="<<c<<endl<<"sub="<<d<<endl
+       <<"mul="<<e<<endl<<"div="<<f<<endl;
+
+return 0;
+}
